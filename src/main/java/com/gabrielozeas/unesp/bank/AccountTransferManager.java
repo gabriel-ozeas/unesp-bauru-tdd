0ead13be76862da0ac071b1f14ca84cd4217690b
Gabriel Ozeas <gabriel.ozeas1@gmail.com>
2012-10-16 06:40:45 -0300

    Removing files

--- src/main/java/com/gabrielozeas/unesp/bank/AccountTransferManager.java
+++
@@ -1,7 +0,0 @@
-package com.gabrielozeas.unesp.bank;
-
-import java.math.BigDecimal;
-
-public interface AccountTransferManager {
-	public void transfer(Account debtAccount, Account creditAccount, BigDecimal value) throws NoMoneyBabyException; 
-}
--- src/main/java/com/gabrielozeas/unesp/bank/NoMoneyBabyException.java
+++
@@ -1,8 +0,0 @@
-package com.gabrielozeas.unesp.bank;
-
-public class NoMoneyBabyException extends Exception {
-	private static final long serialVersionUID = -913761885963835054L;
-	
-	public NoMoneyBabyException(){}
-	public NoMoneyBabyException(String msg) {super(msg);}
-}
