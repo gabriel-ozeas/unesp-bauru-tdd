827579f1fc826fd256968b53faa6527ae886e5a6
Gabriel Ozeas <gabriel.ozeas1@gmail.com>
2012-10-16 20:42:12 -0300

    Cleaning test

--- src/test/java/com/gabrielozeas/unesp/bank/PermissionManagerImplTest.java
+++ src/test/java/com/gabrielozeas/unesp/bank/PermissionManagerImplTest.java
@@ -1,50 +1,6 @@
 package com.gabrielozeas.unesp.bank;
 
-import static org.junit.Assert.assertTrue;
-
-import java.math.BigDecimal;
-
-import org.junit.Test;
-
-import com.gabrielozeas.unesp.bank.domain.Account;
-import com.gabrielozeas.unesp.bank.domain.Client;
-import com.gabrielozeas.unesp.bank.domain.Transaction;
-import com.gabrielozeas.unesp.bank.exception.NotAuthorizatedException;
 
 public class PermissionManagerImplTest {
 	
-	@Test
-	public void maisQue1000ComAutorizacaoDonoReturnTrue() throws Exception {
-		Transaction transaction = new Transaction();
-		transaction.setValue(new BigDecimal("1000"));
-		
-		Client owner = new Client();
-		transaction.setDebtAccount(new Account().setOwner(owner));
-		
-		owner.authorize(transaction);
-		
-		PermissionManagerImpl permission = new PermissionManagerImpl();
-		assertTrue(permission.authorizeTransfer(transaction));
-	}
-	
-	@Test(expected=NotAuthorizatedException.class)
-	public void maisQue1000SemAutorizacaoDonoLanceExcecao() throws Exception {
-		Transaction transaction = new Transaction();
-		transaction.setValue(new BigDecimal("1000"));
-		
-		Client owner = new Client();
-		transaction.setDebtAccount(new Account().setOwner(owner));
-		
-		PermissionManagerImpl permission = new PermissionManagerImpl();
-		permission.authorizeTransfer(transaction);
-	}
-	
-	@Test
-	public void smallValuesReturnTrue() throws Exception {
-		Transaction transaction = new Transaction();
-		transaction.setValue(new BigDecimal("10"));
-		
-		PermissionManagerImpl permission = new PermissionManagerImpl();
-		assertTrue(permission.authorizeTransfer(transaction));
-	}
 }
